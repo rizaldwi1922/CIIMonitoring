4c5aaa6881bf17753f1515d999d8d4d53e3d0411
RizalDwi <rizaldwi2708@gmail.com>
2023-07-09 20:49:24 +0700

    update

--- app/Http/Controllers/Api/AisController.php
+++ app/Http/Controllers/Api/AisController.php
@@ -16,10 +16,28 @@ class AisController extends Controller
         foreach ($ais as $item) {
             $dataDecode = json_decode($item->ais);
             if ($dataDecode->channel == 'A') {
-                $data[] = $dataDecode;
+                $data[] = [
+                    "ais" => $dataDecode,
+                    "date" => $this->ConvertTimeToWIB($item->createdAt)
+                ];
             }
         }
         
         return $data;
+    }
+
+    private static function ConvertTimeToWIB($waktu){
+        // Buat objek DateTime dengan zona waktu asal
+        $waktu_asal = new \DateTime($waktu, new \DateTimeZone('UTC'));
+
+        // Tetapkan zona waktu tujuan sebagai WIB (Asia/Jakarta)
+        $zona_wib = new \DateTimeZone('Asia/Jakarta');
+
+        // Ubah zona waktu ke WIB
+        $waktu_wib = $waktu_asal->setTimezone($zona_wib);
+
+        // Format waktu WIB yang dikonversi
+        $waktu_wib_format = $waktu_wib->format('Y-m-d H:i:s');
+        return $waktu_wib_format;
     }
 }
